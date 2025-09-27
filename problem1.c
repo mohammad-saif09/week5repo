e938d44438d1d5819cba001f07bc3b1c2674a783
Mohammad Saif <azharisaif09@gmail.com>
2025-09-27 08:59:49 -0700

    adding problem1

---
+++ problem1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    printf("Size of integer = %zu byte(s) \n", sizeof(int));
+    printf("Size of character = %zu byte(s) \n", sizeof(char));
+    printf("Size of float = %zu byte(s) \n", sizeof(float));
+    printf("Size of void = %zu byte(s) \n", sizeof(void));
+    printf("Size of long = %zu byte(s) \n", sizeof(long));
+    printf("Size of integer = %zu byte(s) \n", sizeof(int));
+
+
+
+    return 0;
+}
